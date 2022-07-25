503cef68b1b1a0bcd5ae696bb6342c4e9e2c8930
Seth Bailey <phskbailey@gmail.com>
2022-07-25 09:57:04 +0100

    v1.2 tests

--- tests/tests.py
+++
@@ -1,8 +0,0 @@
-from pydatetime.pydatetime.pydatetime import *
-
-print(date_today("dmy"))
-print(time_now(12))
-print(weekday("short"))
-print(current())
-print(day_weekday(15, 8, 2008))
-print(date_in_words("full"))
